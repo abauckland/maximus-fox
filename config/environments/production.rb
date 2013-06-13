5c74bd5a533b4a9a48aa183a385f4b695a572600
Andrew Burton <abauckland@gmail.com>
2013-06-13 22:35:48 +0100

    js and css re-order 3
    change config file to find js and css files to be compiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ SpecrightDev::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( application-main.css application-mobile.css )
+  config.assets.precompile += %w( application-main.css application-main.js application-web.css application-web.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
